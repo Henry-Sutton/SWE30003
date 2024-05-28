bd113e74be8e3658a7fff7b2f851b50479ae511f
Philip <filipposbatistatos@gmail.com>
2024-05-28 21:20:53 +1000

    Fix payments

--- RestaurantApp.py
+++ RestaurantApp.py
@@ -146,7 +146,7 @@ class RestaurantApp:
         tk.Label(order_details_window, text=f"Order Type: {order.order_type}").pack()
         tk.Label(order_details_window, text="Items:").pack()
         for item in order.items:
-            tk.Label(order_details_window, text=f"{item[1]} - ${item[2]}").pack()
+            tk.Label(order_details_window, text=f"{item[0][1]} - ${item[0][2]}").pack()
         ttk.Button(order_details_window, text="Pay", command=lambda: self.pay_order(order)).pack()
 
     def pay_order(self, order):
